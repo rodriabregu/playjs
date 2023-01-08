e89a72202cbd71d3112c89fd3528c60f49a42cd9
Rodrigo Alcino <82095699+rodriabregu@users.noreply.github.com>
2023-01-08 07:58:48 -0300

    feat/first-iteration

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,42 @@
-import Editor from '@monaco-editor/react';
-import { useState } from 'react';
+import { useState } from "react";
+import Editor from "@monaco-editor/react";
 
 function App() {
   const [code, setCode] = useState('');
 
-  try {
-    console.log(eval(code))
-  } catch (error) {  
-    console.log(eval(error))
-  }
+  const resultDiv = document.getElementById('result');
+
+  addEventListener('keyup', () => {
+    resultDiv.innerHTML = '';
+  
+    const lines = code.split('\n');
+  
+    // Iteramos sobre cada línea
+    for (const line of lines) {
+      let result;
+      try {
+        result = eval(line);
+      } catch (error) {
+        result = error;
+      }
+  
+      if (result !== 'undefined') {
+        resultDiv.innerHTML += `${result}<br>`;
+      }
+    }
+  });
 
   return (
-    <div style={{ backgroundColor: 'black'}}>
-      <Editor
-        language='javascript'
-        value='const example = "Hello World";'
-        height={'100vh'}
-        width={'100%'}
-        theme={'vs-dark'}
-        onChange={e => setCode(e.toString())}
-      />
+    <div style={{
+      display: 'flex',
+      flexDirection: 'row',
+      width: '100vw',
+      height: '100vh',
+      padding: '2rem'
+
+    }}>
+      <Editor line={2} width={'50vw'} defaultLanguage="javascript" onChange={e => setCode(e)} />
+      <span id='result' />
     </div>
   );
 }
