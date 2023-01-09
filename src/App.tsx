4ea078908055580e55c3cd607677c0b86ca2b3e5
Rodrigo Alcino <82095699+rodriabregu@users.noreply.github.com>
2023-01-08 23:09:02 -0300

    feat/eval-event

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,64 @@
-import { useState } from "react";
-import Editor from "@monaco-editor/react";
+import { useState } from 'react';
+import Editor from '@monaco-editor/react';
 
 function App() {
   const [code, setCode] = useState('');
-
   const resultDiv = document.getElementById('result');
 
+  // function evaluarCodigo(codigo, elemento) {
+  //   let output = document.querySelector(elemento);
+
+  //   output.innerHTML = '';
+  //   try {
+  //     let funcion = function() {
+  //       return eval(codigo);
+  //     };
+
+  //     let contexto = {};
+  //     output.innerHTML += funcion.apply(contexto) + "<br>";
+  //   } catch (error) {
+  //     output.innerHTML += error + "<br>";
+  //   }
+  // }
+
   addEventListener('keyup', () => {
-    resultDiv.innerHTML = '';
-  
+    window.eval(code);
+    if (resultDiv) resultDiv.innerHTML = '';
+
     const lines = code.split('\n');
-  
-    // Iteramos sobre cada línea
+
     for (const line of lines) {
       let result;
       try {
-        result = eval(line);
+        result = eval(line)
       } catch (error) {
         result = error;
       }
-  
-      if (result !== 'undefined') {
-        resultDiv.innerHTML += `${result}<br>`;
-      }
+
+      if (result === undefined) result = '';
+      if (resultDiv) resultDiv.innerHTML += `${result}<br>`;
     }
   });
 
   return (
-    <div style={{
-      display: 'flex',
-      flexDirection: 'row',
-      width: '100vw',
-      height: '100vh',
-      padding: '2rem'
-
-    }}>
-      <Editor line={2} width={'50vw'} defaultLanguage="javascript" onChange={e => setCode(e)} />
-      <span id='result' />
+    <div
+      style={{
+        backgroundColor: '#282c34',
+        color: 'white',
+        display: 'flex',
+        flexDirection: 'row',
+        width: '100vw',
+        height: '100vh',
+      }}
+    >
+      <Editor
+        width={'50vw'}
+        defaultLanguage='javascript'
+        // onChange={(e) => setCode(e?.replace('const', 'var') ?? '')}
+        onChange={(e) => setCode(e ?? '')}
+        theme={'vs-dark'}
+      />
+      <div style={{ marginLeft: '1rem' }} id='result' />
     </div>
   );
 }
